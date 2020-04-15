e2abb8c1af15f0b7b1c71bf3f3d4935d2e1c4198
Abhishek mishra <44899517+akm0786@users.noreply.github.com>
2020-04-15 10:50:06 +0530

    icon added

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>INDIA COVID 19</title>
+	<link rel="shortcut icon" type="image/png" href="images.jpg">
 	 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
